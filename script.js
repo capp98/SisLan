8c241ea0f770a1d64c43b7dd30d9a332b8842f88
capp98 <54820067+capp98@users.noreply.github.com>
2023-09-22 12:44:53 -0300

    1 file modified

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let servicos = await fetch('https://sislan-api.onrender.com/services/serviço')
+  .then((response) => response.json())
+  .then((data) => data);
+
+let boletos = await fetch('https://sislan-api.onrender.com/services/boleto')
+  .then((response) => response.json())
+  .then((data) => data);
+
+let documentos = await fetch(
+  'https://sislan-api.onrender.com/services/documento'
+)
+  .then((response) => response.json())
+  .then((data) => data);
+
 document.getElementById('windowInfos').addEventListener('click', function (e) {
   if (e.target.id == 'windowInfos') e.target.style.display = 'none';
 });
@@ -27,6 +41,8 @@ function search(word) {
 }
 
 window.onload = function () {
+  console.log(boletos);
+
   adicionaBoletos();
 
   adicionaDocumentos();
@@ -50,7 +66,8 @@ function adicionaBoletos() {
 
   let link = null;
 
-  boletos.sort(ordenaPorNome);
+  console.log(boletos);
+
   boletos.forEach((boleto) => {
     link = document.createElement('a');
 
@@ -238,319 +255,3 @@ function showInfos(e) {
     document.getElementById('windowInfos').style.display = 'block';
   }
 }
-
-let boletos = [
-  {
-    url: 'https://www.siscred.com.br/associados/login.aspx',
-    nome: 'Siscred/regicred',
-    servico: 'boleto',
-    tipo: 'cartão',
-  },
-  {
-    url: 'https://scimpmgsp.geometrus.com.br/mamb_lancamentos/segunda_via_boleto',
-    nome: 'Ambulante',
-    servico: 'boleto',
-    tipo: 'prefeitura',
-  },
-  {
-    url: 'https://scimpmgsp.geometrus.com.br/rf30',
-    nome: 'REFIS',
-    servico: 'boleto',
-    tipo: 'prefeitura',
-  },
-  {
-    url: 'https://web.whatsapp.com/send/?phone=5513997786129&text=Oi',
-    nome: 'master net (whatsapp)',
-    servico: 'boleto',
-    tipo: 'internet',
-  },
-  {
-    url: 'http://central.niufibra.com.br:8080/sac/login/?sys=SAC',
-    nome: 'niufibra',
-    servico: 'boleto',
-    tipo: 'internet',
-  },
-  {
-    url: 'https://web.whatsapp.com/send/?phone=551340099070&text=Oi',
-    nome: 'niufibra (whatsapp)',
-    servico: 'boleto',
-    tipo: 'internet',
-  },
-  {
-    url: 'https://sac.desktop.com.br/Cliente_Documento.jsp',
-    nome: 'desktop',
-    servico: 'boleto',
-    tipo: 'internet',
-  },
-  {
-    url: 'https://web.whatsapp.com/send/?phone=551935143100&text=Oi',
-    nome: 'desktop (whatsapp)',
-    servico: 'boleto',
-    tipo: 'internet',
-  },
-  {
-    url: 'https://intranet.intervel.com.br/central_assinante_web/',
-    nome: 'intranet connect',
-    servico: 'boleto',
-    tipo: 'internet',
-  },
-  {
-    url: 'https://agencia.neoenergiaelektro.com.br/login.aspx',
-    nome: 'elektro',
-    servico: 'boleto',
-    tipo: 'governo',
-  },
-  {
-    url: 'https://casasbahia.digital/',
-    nome: 'casas bahia',
-    servico: 'boleto',
-    tipo: 'cartão',
-  },
-  {
-    url: 'https://fatura-facil.claro.com.br/',
-    nome: 'claro',
-    servico: 'boleto',
-    tipo: 'celular',
-  },
-  {
-    url: 'https://scimpmgsp.geometrus.com.br/digits',
-    nome: 'IPTU (Guarujá)',
-    servico: 'boleto',
-    tipo: 'governo',
-  },
-  {
-    url: 'http://www8.receita.fazenda.gov.br/SimplesNacional/Aplicacoes/ATSPO/pgmei.app/Identificacao',
-    nome: 'MEI',
-    servico: 'boleto',
-    tipo: 'governo',
-  },
-  {
-    url: 'https://www.enel.com.br/pt/login',
-    nome: 'enel',
-    servico: 'boleto',
-    tipo: '',
-  },
-  {
-    url: 'https://sal.rfb.gov.br/PortalSalInternet/faces/pages/calcContribuicoesCI/filiadosApos/exibirDadosCadastraisCIApos.xhtml',
-    nome: 'SAL (sistema de acréscimos legais)',
-    servico: 'boleto',
-    tipo: 'governo',
-    extras: {
-      instrucoes:
-        'Contribuinte Individual (1007, 1163, 1120, 1236, 1287, 1805)',
-    },
-  },
-  {
-    url: 'https://pf.santandernet.com.br/LOGBBR_NS_ENS/ChannelDriver.ssobto?dse_contextRoot=true#',
-    nome: 'santander',
-    servico: 'boleto',
-    tipo: 'banco',
-  },
-  {
-    url: 'https://www.portaldafatura.com.br/boleto-rapido/login',
-    nome: 'credsystem',
-    servico: 'boleto',
-    tipo: 'cartão',
-  },
-  {
-    url: 'https://cdhuonline.cdhu.sp.gov.br/',
-    nome: 'CDHU',
-    servico: 'boleto',
-    tipo: 'governo',
-  },
-  {
-    url: 'https://fatura.credz.com.br/login',
-    nome: 'CredZ',
-    servico: 'boleto',
-    tipo: 'cartão',
-  },
-  {
-    url: 'http://anacostasaude.com.br/boleto_acesso.php',
-    nome: 'tim',
-    servico: 'boleto',
-    tipo: 'celular',
-  },
-  {
-    url: 'https://empresas.unimedsantos.coop.br/auth/login',
-    nome: 'unimed',
-    servico: 'boleto',
-    tipo: '',
-  },
-  {
-    url: 'https://www.poupatempo.sp.gov.br/wps/myportal/poupatempoTaOn/servicos/sabesp/segundaViaContaSabesp/',
-    nome: 'sabesp',
-    servico: 'boleto',
-    tipo: 'poupatempo',
-  },
-];
-
-let documentos = [
-  {
-    url: 'https://www.poupatempo.sp.gov.br/wps/myportal/poupatempoTaOn/listaRamos?arvId=2495#Z7_NHD2H4G0O0P5506KRJBM2P3OI3',
-    nome: 'agendar 2ª via do RG',
-    servico: 'documento',
-    tipo: 'poupatempo',
-    extras: {
-      preco: 'R$ 15,00; R$ 20,00 se nao souber a conta gov',
-      necessario: 'Acessar pelo Edge',
-    },
-  },
-  {
-    url: 'https://www.poupatempo.sp.gov.br/wps/myportal/poupatempoTaOn/servicos/gerar-atestado-de-antecedentes-criminais',
-    nome: 'antecedente criminal estadual (poupatempo)',
-    servico: 'documento',
-    tipo: 'poupatempo',
-  },
-  {
-    url: 'https://www.tse.jus.br/servicos-eleitorais/certidoes/certidao-de-quitacao-eleitoral',
-    nome: 'quitação eleitoral',
-    servico: 'documento',
-    tipo: 'titulo eleitoral',
-  },
-  {
-    url: 'https://entregadeexames.com.br/',
-    nome: 'entrega de exames',
-    servico: 'documento',
-    tipo: 'exames',
-  },
-  {
-    url: 'https://esaj.tjsp.jus.br/sco/abrirCadastro.do',
-    nome: 'certidão de execução criminal',
-    servico: 'documento',
-    tipo: '',
-  },
-  {
-    url: 'http://www2.ssp.sp.gov.br/atestado/novo/Atestado02.cfm',
-    nome: 'antecedente criminal estadual',
-    servico: 'documento',
-    tipo: '',
-    extras: {
-      preco: 'R$ 5,00',
-      necessario: 'RG mais recente',
-    },
-  },
-  {
-    url: 'https://antecedentes.dpf.gov.br/antecedentes-criminais/certidao',
-    nome: 'antecedente criminal federal',
-    servico: 'documento',
-    tipo: '',
-  },
-  {
-    url: 'http://www.radarconsultas.com.br/',
-    nome: 'radar consultas',
-    servico: 'documento',
-    tipo: '',
-    extras: {
-      preco: 'R$ 50,00',
-      entrega: 'Resultados do SERASA, SCPC, CARTÓRIO, SCORE',
-    },
-  },
-  {
-    url: 'https://servicos.receita.fazenda.gov.br/Servicos/CPF/ConsultaSituacao/ConsultaPublica.asp',
-    nome: 'situação cadastral do cpf',
-    servico: 'documento',
-    tipo: '',
-  },
-  {
-    url: 'http://bopm.policiamilitar.sp.gov.br/',
-    nome: 'boletim de ocorrência militar',
-    servico: 'documento',
-    tipo: 'policia militar',
-  },
-  {
-    url: 'https://www.delegaciaeletronica.policiacivil.sp.gov.br/ssp-de-cidadao/home',
-    nome: 'boletim de ocorrência',
-    servico: 'documento',
-    tipo: '',
-  },
-  {
-    url: 'https://sisparnet.pgfn.fazenda.gov.br/sisparInternet/internet/darf/consultaParcelamentoDarfInternet.xhtml',
-    nome: 'DARF',
-    servico: 'documento',
-    tipo: '',
-  },
-  {
-    url: 'https://esaj.tjsp.jus.br/esaj/portal.do?servico=190090',
-    nome: 'processos de justiça',
-    servico: 'documento',
-    tipo: '',
-  },
-  {
-    url: 'https://www.detran.sp.gov.br/wps/myportal/portaldetran/cidadao/veiculos/servicos/solicitacaoLicenciamentoCRLVDigital/!ut/p/z1/pZHLDoIwEEU_qbe0Ie2yYqG8gvgA7MawMk0UXRi_X2FnohXj7CY5Z2Zyh1jSETv0d3fsb-4y9Kdnv7fhgVOpKY1QVo3RqE1s6AoCSEDaCWAcG7mgCmBGQIn11qgCFDwk9h8_4T_6Qjfx6LOY6ipAjnl-ViMyRgRFQpcRlOS8TLOcQbJ5Pj6UmrnfA1j_-JbYCfF9wAuMEb8CbzKcAF9I3868nndjdXCpewBJp-N9/p0/IZ7_419E11C0MOVHE0QHFH1P080040=CZ6_419E11C0MOVHE0QHFH1P0800G0=LAJyVsOUda88e_ef0lSMUbB4hXrPWHSJ9q78R9UUcGbEbPHgd9A=Ejavax.servlet.include.path_info!QCPpagesQCPlicenciamentoCRLVDigitalQCPprePesquisaView.xhtml==/#Z7_419E11C0MOVHE0QHFH1P080040',
-    nome: 'CRLV-E licenciamento de veículo',
-    servico: 'documento',
-    tipo: 'detran',
-    extras: {
-      preco: 'R$ 10,00 ; R$ 20,00 se não souber a conta Detran',
-      necessario: 'Acesso ao celular ou e-mail do proprietário',
-      entrega: 'Duas cópias do documento',
-    },
-  },
-  {
-    url: 'https://www.detran.sp.gov.br/wps/myportal/portaldetran/cidadao/habilitacao/servicos/solicitacaoCertidaoProntuario',
-    nome: 'certidão de prontuário',
-    servico: 'documento',
-    tipo: 'detran',
-    extras: {
-      preco: 'R$ 5,00 ; R$ 20,00 se não souber a conta Detran',
-    },
-  },
-  {
-    url: 'https://www.detran.sp.gov.br/wps/myportal/portaldetran/cidadao/habilitacao/servicos/consultaPontos',
-    nome: 'certidão de pontos na CNH',
-    servico: 'documento',
-    tipo: 'detran',
-    extras: {
-      preco: 'R$ 5,00;R$ 20,00 se não souber a conta Detran',
-    },
-  },
-];
-
-let servicos = [
-  {
-    url: 'https://mei.receita.economia.gov.br/baixa/acesso',
-    nome: 'fechar MEI',
-    servico: 'serviço',
-    tipo: 'governo;MEI',
-    extras: {
-      preco: 'R$ 20,00',
-    },
-  },
-  {
-    url: '/SisLan/pages/cv.html',
-    nome: 'currículo',
-    servico: 'serviço',
-    tipo: 'documento',
-    extras: {
-      preco: 'R$ 8,00 - PDF ou 5 Cópias;R$ 10,00 - PDF + 5 Cópias',
-    },
-  },
-  {
-    url: '/SisLan/pages/proof-of-residence.html',
-    nome: 'declaração de residência',
-    servico: 'serviço',
-    tipo: 'documento',
-    extras: {
-      preco: 'R$ 10,00',
-      entrega: '2 Cópias + Envelope',
-    },
-  },
-  {
-    url: 'https://online-audio-converter.com/pt/',
-    nome: 'conversor de áudio',
-    servico: 'serviço',
-    tipo: 'conversor',
-  },
-  {
-    url: 'https://www.ilovepdf.com/pt',
-    nome: 'editor de pdf',
-    servico: 'serviço',
-    tipo: 'conversor',
-  },
-  {
-    url: 'https://www.tse.jus.br/servicos-eleitorais/autoatendimento-eleitoral#/',
-    nome: 'Autoatendimento eleitoral',
-    servico: 'serviço',
-    tipo: 'conversor',
-  },
-];
